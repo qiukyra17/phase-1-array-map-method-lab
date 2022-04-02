7d03216d255bd6d00e09e9ebfba3d19532f3482f
kyra qiu <kyraqiu@gmail.com>
2022-04-02 17:51:27 -0400

    commit tree 618914c48f5c688ee6b943140145a99b2259de8f

--- index.js
+++ index.js
@@ -11,6 +11,61 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   return tutorials
+// }
+
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {const sentence = tutorial.split(' ').map((word)=>word[0].toUpperCase() + word.slice(1))
+  return sentence.join(' ');
+})}
+
+
+// function titledCased(){
+//   return tutorials.map(tutorial => {const sentence = tutorial.split(' ').map((word)=>word[0].toUpperCase() + word.slice(1))
+//   return sentence;
+// })}
+
+console.log(titledCased);
+// const arrJoined = tutorials.join(" ");
+// const strSplit = arrJoined.split(" ");
+
+// function capitalize (element){
+//     let singleC = element.map(elem => elem[0].toUpperCase() + elem.substring(1));
+//     return [singleC.join(" ")];
+//   }
+
+// console.log(arrJoined);
+// console.log(strSplit);
+// console.log(capitalize(strSplit));
+
+// const titleCased = () => {
+  //   let big = tutorials.mapp(arr.toLowerCase().split(',').map(elem => {
+  //       return elem[0].toUpperCase()+elem.slice(1)
+  //     }))
+  //     return big.join(" ");
+  //   }
+
+// function makeSmallArray (arr,size){
+//   const smallArr = [];
+//   while (arr.length>0){
+//     const single = arr.splice(0,size);
+//     smallArr.push(single);
+//   }
+//   return smallArr;
+// }
+
+
+// const changeToStrings = tutorials.toString(" ")
+// const changeToWords = changeToStrings.split(" ");
+
+// function capitalize (element){
+//   let singleC = element.map(elem => elem[0].toUpperCase() + elem.substring(1));
+//   return [singleC.join(" ")];
+// }
+
+
+// console.log(changeToStrings);
+// console.log(changeToWords);
+// console.log(capitalize(changeToWords));
